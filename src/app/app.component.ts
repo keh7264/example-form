6d0e07fb380e977aa32eb70f502b831b1d14c563
ehkim <keh7264@gmail.com>
2018-11-19 22:31:38 +0900

    reactive: creating a form in code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,26 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('f') signUpForm: NgForm;
-  defaultQuestion = 'pet';
-  answer = '';
-  gentders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    gender: '',
-    answer: ''
-  }
-  submitted: boolean = false;
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signUpForm: FormGroup;
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.signUpForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'male'
-    // });
-    this.signUpForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl('female')
     });
   }
 
-  // onSubmit(form: NgForm) {
-  //   console.log(form);
-  // }
-
   onSubmit() {
     console.log(this.signUpForm);
-    this.submitted = true;
-    this.user.username = this.signUpForm.value.userData.username;
-    this.user.email = this.signUpForm.value.userData.email;
-    this.user.secretQuestion = this.signUpForm.value.secret;
-    this.user.gender = this.signUpForm.value.gender;
-    this.user.answer = this.signUpForm.value.questionAnswer;
-    this.signUpForm.reset();
   }
-
 }
